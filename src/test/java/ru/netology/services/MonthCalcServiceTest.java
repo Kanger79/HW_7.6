7afdeec5dc7ddbb89f5412d1e1d09970dcbbc4f3
Dmitry Kanger <dmitrykanger@yandex.ru>
2023-04-16 18:10:18 +0300

    Make_ParametrizedTest

--- src/test/java/ru/netology/services/MonthCalcServiceTest.java
+++ src/test/java/ru/netology/services/MonthCalcServiceTest.java
@@ -2,36 +2,42 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 // import ru.netology.services.MonthCalcService;
 
 public class MonthCalcServiceTest {
 
-    @Test
-    void exampleOne() {
+    @ParameterizedTest
+    @CsvSource({
+            "10000, 3000, 20000, 3",
+            "100000, 60000, 150000, 2"
+    })
+    void exampleOne(int income, int expenses, int threshold, int expected) {
         MonthCalcService service = new MonthCalcService();
         // входные данные:
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
+//        int income = 10_000;
+//        int expenses = 3_000;
+//        int threshold = 20_000;
+//        int expected = 3;
 
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Test
-    void exampleTwo() {
-        MonthCalcService service = new MonthCalcService();
+//    @Test
+//    void exampleTwo() {
+//        MonthCalcService service = new MonthCalcService();
         // входные данные:
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
+//        int income = 100_000;
+//       int expenses = 60_000;
+//        int threshold = 150_000;
+//        int expected = 2;
 
-        int actual = service.calculate(income, expenses, threshold);
-        Assertions.assertEquals(expected, actual);
+//        int actual = service.calculate(income, expenses, threshold);
+//        Assertions.assertEquals(expected, actual);
 
-    }
+//    }
 
 }
